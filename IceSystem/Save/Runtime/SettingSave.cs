2c8222c2b70c6b6e77e1d2cad96bc85a06088fe4
Minstreams <32974920+Minstreams@users.noreply.github.com>
2022-09-25 23:30:20 +0800

    Default Color

--- IceSystem/Save/Runtime/SettingSave.cs
+++ IceSystem/Save/Runtime/SettingSave.cs
@@ -3,6 +3,9 @@
     [IceSettingPath("IceEngine/IceSystem/Save")]
     public class SettingSave : Framework.IceSetting<SettingSave>
     {
-
+        public SettingSave()
+        {
+            themeColor = new UnityEngine.Color(0.4773f, 0.7075f, 0.4953f);
+        }
     }
 }
